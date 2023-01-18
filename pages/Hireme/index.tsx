e762cb5eadd1d0c0ce0c65ec7d646911448cab3f
muzamil077 <meixuhussain.007@gmail.com>
2023-01-18 19:24:21 +0500

    changes

---
+++ pages/Hireme/index.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @next/next/no-img-element */
+import { content } from "../../utills/Content";
+import Image from "next/image";
+
+const Hireme = () => {
+  const { Hireme } = content;
+
+  return (
+    <section className="bg-bg_light_primary">
+      <div className="md:container px-5 pt-14">
+        <h2 className="title" data-aos="fade-down">
+          {Hireme.title}
+        </h2>
+        <h4 className="subtitle" data-aos="fade-down">
+          {Hireme.subtitle}
+        </h4>
+        <br />
+        <div className="flex items-center md:flex-row flex-col-reverse ">
+          <Image width={300} height={500} src={Hireme.image1} alt={"hireme"} />
+
+          <Image width={300} height={500} src={Hireme.image2} alt={"hireme"} />
+
+          <div
+            data-aos="fade-left"
+            className="border-2 border-dark_primary max-w-sm
+           p-6 shadow-sm rounded-xl rounded-br-[8rem] sm:min-w-[22rem]"
+          >
+            <p className="leading-7">{Hireme.para}</p>
+            <br />
+            <button className="btn bg-dark_primary text-white">
+              {Hireme.btnText}
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hireme;
---
+++ src/components/Topbar.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import { useState } from 'react'
+import { content } from '../../utills/Content'
+import { HiMenuAlt2 } from 'react-icons/hi'
+import { createElement } from 'react'
+
+const Topbar = () => {
+  const { nav } = content
+  const [showMenu, setShowMenu] = useState(false)
+  const [active, setActive] = useState(0)
+
+  return (
+    <div className="w-full bg-red-500 flex justify-center">
+      <div
+        className="sm:cursor-pointer fixed top-10 left-10 z-[999] rounded-lg bg-white/40 p-2"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <HiMenuAlt2 size={34} />
+      </div>
+      <nav
+        className={`fixed  z-[999] flex items-center gap-5 bg-slate-200/60 px-6 py-3 backdrop-blur-md rounded-full text-dark_primary duration-300 ${
+          showMenu ? 'bottom-10' : 'bottom-[-100%]'
+        }`}
+      >
+        {nav.map((item, i) => (
+          // eslint-disable-next-line react/jsx-key
+          <a
+            href={item.link}
+            onClick={() => setActive(i)}
+            className={`text-xl p-2.5 rounded-full sm:cursor-pointer 
+     ${i === active && 'bg-dark_primary text-white'} `}
+          >
+            {createElement(item.icon)}
+          </a>
+        ))}
+      </nav>
+    </div>
+  )
+}
+
+export default Topbar;
